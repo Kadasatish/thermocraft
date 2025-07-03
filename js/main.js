fd5787c0d437e3e8c2be47593e22eb4d6579d4e7
Kadasatish <kadasatish2022@gmail.com>
2025-07-03 21:01:28 +0530

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function showPaymentSuccess(dotId) {
+  const dot = document.getElementById(dotId);
+  dot.style.backgroundColor = 'white';
+  setTimeout(() => {
+    dot.style.backgroundColor = 'black';
+  }, 1500);
+}
